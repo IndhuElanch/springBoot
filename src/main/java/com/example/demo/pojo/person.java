049bbbfdf2096c17e0652cff38e05a925c10b667
IndhuElanch <indhuelanch@gmail.com>
2024-06-24 01:51:06 +0530

    Trial spring boot project

---
+++ src/main/java/com/example/demo/pojo/person.java
@@ -0,0 +1,13 @@
+package com.example.demo.pojo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+public class person {
+
+    int id;
+    String name;
+    int age;
+
+}
